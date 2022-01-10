4c75a7154725cf8f25148e9b4ab32bd24431c0f4
miki0033 <75072281+miki0033@users.noreply.github.com>
2022-01-10 21:41:02 +0100

    Update listePCB.c

--- phase1/listePCB/listePCB.c
+++ phase1/listePCB/listePCB.c
@@ -2,7 +2,7 @@
  * listePCB.c
  *
  *  Created on: 8 gen 2022
- *      Author: Pc
+ *      Author: MM
  */
 
 #include "listePCB.h"
@@ -12,26 +12,35 @@
 void mkEmptyProcQ(struct list_head *head){ //4
 /*Crea una lista di PCB, inizializzandola
 come lista vuota */
-	head->p_next=NULL;
-	head->p_head=head;
-	//head->p_tail=head;
+	pcb_t *q=head;
+	q->p_next=NULL;
+	q->p_parent=NULL;
+	q->p_child=NULL;
+	q->p_sib=NULL;
+	q->p_s=0;
+	q->p_time=0;
+	q->p_semAdd=NULL;
 
-	//aggiungere in caso altri campi da inizioalizzare head->"campo"=0;
+	//aggiungere in caso altri campi da inizializzare head->"campo"=0;
 
 }
 
 int emptyProcQ(struct list_head *head){ //5
 /*Restituisce TRUE se la lista puntata da
 head è vuota, FALSE altrimenti. */
-	if(head->p_next == NULL) return TRUE;
+	if(head== NULL) return TRUE;
 	else return FALSE;
 }
 
-void insertProcQ(struct list_head* head, pcb_t *p){ //6
+void insertProcQ(struct list_head* head, pcb_t* p){ //6
 /*Inserisce l’elemento puntato da p nella
 coda dei processi puntata da head*/
-	head->p_next = p;	//p2->next = new pcb_t;
-	//head->p_tail = p;
+	pcb_t q;
+	q=head;
+	while(q->p_next!=NULL){ //scorre la lista fino alla fine
+		q=q->p_next;
+	}
+	q->p_next = p;	//p2->next = new pcb_t;
 }
 
 pcb_t headProcQ(struct list_head* head){ //7
@@ -39,7 +48,7 @@ pcb_t headProcQ(struct list_head* head){ //7
 dei processi da head, SENZA
 RIMUOVERLO. Ritorna NULL se la coda
 non ha elementi. */
-	if(head->p_head!=NULL) return head->p_head;
+	if(head!=NULL) return head;
 	else return NULL;
 }
 
@@ -48,22 +57,18 @@ pcb_t* removeProcQ(struct list_head *head){ //8
 processi puntata da head. Ritorna NULL se la
 coda è vuota. Altrimenti ritorna il puntatore
 all’elemento rimosso dalla lista */
-	list_head *p;
-	list_head *newhead;
-	if (head==NULL) return NULL;	//controlla se la lista è vuota
-	else if (head->p_next==NULL) return head;	//controlla se ci sono altri elementi oltre la testa
+	pcb_t *p;
+	pcb_t *newhead;
+	p=head;	//salva il puntatore alla testa in un puntatore p temporaneo
+	if (p==NULL) return NULL;	//controlla se la lista è vuota
+	else if (p->p_next==NULL) {	//controlla se ci sono altri elementi oltre la testa
+		head=NULL;
+		return p;
+	}
 	else {
-		p = head ;	//salva il puntatore alla testa in un puntatore p temporaneo
 		newhead=p->p_next;	//salva il puntatore al elemento successivo alla testa, che sarà la nuova testa
-		p=p->p_next;
-		while(p->p_next!=NULL){	//assegna l'indirizo alla nuova testa a tutti i nodi della coda
-			p->head=newhead;
-			p=p->p_next;
-		}
-		head->p_next=NULL;	//pulisce i campi del nodo rimosso
-		head->p_head=NULL;
-
-		return head;
+		head=newhead;
+		return p;
 }
 
 pcb_t* outProcQ(struct list_head *head, pcb_t *p){ //9
@@ -71,23 +76,23 @@ pcb_t* outProcQ(struct list_head *head, pcb_t *p){ //9
 processi puntata da head. Se p non è presente
 nella coda, restituisce NULL. (NOTA: p può
 trovarsi in una posizione arbitraria della coda). */
-	list_head *tmp;
-	list_head *tmpbefore;
-	list_head *rt=NULL;
-	if(head == p){ // controlla se il pcb da togliere è in testa
+	pcb_t *tmp;
+	pcb_t *tmpbefore;
+	pcb_t *rt=NULL;
+	tmp=head;
+	if(tmp == p){ // controlla se il pcb da togliere è in testa
 		rt=removeProcQ(p); //quindi richiama la funzione per rimuovere il primo elemento della coda
-		
+
 	}
 	else{
-		if(head->p_next!=NULL){	//controlla che ci siano elementi dopo il primo
-			tmp=head->p_next;	//puntatore da fare scorrere della lista
+		if(tmp->p_next!=NULL){	//controlla che ci siano elementi dopo il primo
+			tmp=tmp->p_next;	//puntatore da fare scorrere della lista
 			tmpbefore = head;	//puntatore al elemento precedente a quello puntato da tmp
 			while(tmp->p_next!=NULL){ //controlla se il PCB da togliere è in mezzo alla coda
 
 				if(tmp==p){
 					tmpbefore->p_next = tmp->p_next; //fa puntare il blocco precedente a p a quello successivo a p
 					tmp->p_next=NULL;	//toglie il puntatore al blocco successivo a p
-					tmp->p_head=NULL;	//toglie il puntatore alla testa della lista
 					rt=p;
 				}
 				tmpbefore = tmpbefore->p_next; //scorrono i puntatori per far andare avanti il ciclo
