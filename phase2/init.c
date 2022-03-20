3dfea4952d7b094928dd9678100b1acf650e73de
Matteo <matteomanuelli@protonmail.com>
2022-03-20 23:52:10 +0100

    init finitoe impostato lo scheduling

--- phase2/init.c
+++ phase2/init.c
@@ -1,4 +1,4 @@
-#include "../pandos_const.h"
+#include "../pandos_const.h" //per dubbi cerca "?"
 #include "../pandos_types.h"
 #include "pcb.h"
 #include "asl.h"
@@ -18,9 +18,9 @@ int softBlockCount;
 that are in the “ready” state. */
 //ricordarsi che quando un processo ad alta priorità esegue una yield(), bisogna
 //cercare di far eseguire gli altri, anche se è il primo della high priority q
-struct list_head readyQueueHighPriority;
-unsigned int lastProcessHasYielded[1][1]; //solo per processi ad alta priorità. unsigned int perchè lo è memaddr. nel primo ci sarà il pid,
-//nel secondo ci sarà un booleano
+struct list_head readyQueueHighPriority; //capire TAIL
+unsigned int lastProcessHasYielded[1][1] = [NULL][NULL]; //solo per processi ad alta priorità. unsigned int perchè lo è memaddr. nel primo ci sarà il pid,
+//nel secondo ci sarà un booleano. NULL = unsigned int 0 il quale indirizzo non potrà/dovrà esistere
 /* Tail pointer to a queue of pcbs (related to low priority processes) 
 that are in the “ready” state. */
 struct list_head readyQueueLowPriority;
@@ -58,10 +58,39 @@ int main() {
 	for (int i = 0; i < NoDEVICE; i++) deviceSemaphores[i] = 0;
 
 	//load interval timer globale. pg 21 pdf capitolo rivisto
-	LDIT(PSECOND); //100000
+	LDIT(PSECOND); //100000 - ?"scrivendolo nel registro corrispondente" ci serve un indirizzo? DEV2ON 0x00000004 (dubbio sorto dalle slide,
+	// sul libro è sciallato e non c'è nessun accenno a ciò)
 
 	pcb_PTR initProc = AllocPcb();
+	initProc->p_time = 0;
+	initProc->p_semAdd = NULL;
+	initProc->p_supportStruct = NULL;
+	initProc->p_parent = NULL; //"set all the process Tree fields to NULL"
+	initProc->p_child = NULL;
+	initProc->p_sib = NULL;
+	initProc->p_prio = 0; //poichè viene inserito in una coda a bassa priorità. gestire già la politica di assegnamento dei pid? (puntatore
+	// univoco a struttura pcb_t)
+
 	processCount+=1;
 
+	/*init first process state */
+	state_t initState;
+	STST(&initState); //salvare stato processore in una struttura pcb_t
+
+	RAMTOP(initState.reg_sp); //SP a ramtop
+
+    /*process needs to have interrupts enabled, the processor Local Timer enabled, kernel-mode on => Status register constants */
+    initState.status = IEPON | IMON | TEBITON //parte non capita appieno https://github.com/AndreSkin/progetto_PandOs/blob/main/Fase_2/initial.c
+    //2.3 nel libro (non student guide). cosa significano?
+
+    initState.pc_epc = (memaddr) test; /*(exec) PC all'indirizzo della funzione test di p2test */
+    initState.reg_t9 = (memaddr) test; //idiosincrasia dell'architettura
+
+    initProc->p_s = initState;
+
+    insertProcQ(&(readyQueueLowPriority), initProc);
+
 	scheduler();
+
+	return 0;
 }
--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -1,3 +1,26 @@
+#include "../pandos_const.h"
+#include "../pandos_types.h"
+#include "pcb.h"
+#include "asl.h"
+#include "main.h"
+
+extern struct list_head readyQueueLowPriority;
+extern struct list_head readyQueueHighPriority;
+extern int softBlockCount;
+extern int processCount;
+extern pcb_PTR currentProcess;
+
+cpu_t startTime;
+
 void scheduler() {
-	
+	if (currentProcess != NULL) {
+		
+	}
+	//get next process to run, list_empty, flag se bassa/alta priorità
+	if (currentProcess != NULL) {
+
+	}
+	else {
+
+	}
 }
