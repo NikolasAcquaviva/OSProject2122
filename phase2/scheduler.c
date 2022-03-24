68f5f3aaf0e36517b10e50393ecf89a4598cfeb8
cosimopp <manuelli.matteo01@gmail.com>
2022-03-24 14:58:30 +0100

    update

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -4,23 +4,49 @@
 #include "asl.h"
 #include "main.h"
 
-extern struct list_head readyQueueLowPriority;
-extern struct list_head readyQueueHighPriority;
+extern struct list_head LowPriorityReadyQueue;
+extern struct list_head HighPriorityReadyQueue;
 extern int softBlockCount;
 extern int processCount;
 extern pcb_PTR currentProcess;
-
+unsigned int lastHighPriorityProcessHasYielded;
 cpu_t startTime;
 
 void scheduler() {
-	if (currentProcess != NULL) {
-		
+	if (currentProcess != NULL) { // => c'è già un processo in exec
+		//pg 32/158 per capire STCK(x) populate x
 	}
-	//get next process to run, list_empty, flag se bassa/alta priorità
+	//get next process to run, list_empty, flag se bassa/alta priorità ...
 	if (currentProcess != NULL) {
 
-	}
-	else {
+		//se l'ultimo processo era ad alta priorità e ha rilasciato le risorse,
+		if (lastHighPriorityProcessHasYielded != NULL) {
 
+			pcb_PTR headHighPriorityQueue = headProcQ(&HighPriorityReadyQueue);
+
+			//e se è il primo processo nella coda ad alta priorità pronto per essere eseguito (puntatore pcb = pid processo)
+			if (headHighPriorityQueue == lastHighPriorityProcessHasYielded) {
+
+				//controlla che non sia l'unico, ovvero che il suo next non sia la sentinella
+				//ma poichè non sappiamo l'indirizzo della sentinella, controlliamo che il next del next
+				//non sia il processo che ha rilasciato la risorsa
+				if (container_of(headHighPriorityQueue->next.next, pcb_t, p_list) == headHighPriorityQueue) {
+
+					//controlla che la coda low priority non sia vuota. Se non è vuota fai partire il suo primo processo in attesa
+					if (list_empty(LowPriorityReadyQueue)){
+
+					}
+					//best effort: fai partire il processo che aveva rilasciato le risorse
+					else
+				}
+				//altrimenti fai partire il secondo processo ad alta priorità ready
+				else 
+			}
+			//altrimenti come consuetudine fai partire il processo con più alta priorità in attesa
+			else 
+		}
+		//altrimenti come consuetudine fai partire il processo con più alta priorità in attesa
+		else 
 	}
+	else{}
 }
