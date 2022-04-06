1de6260c4ffcccabdba354e25a0580eaa34f14ba
cosimopp <manuelli.matteo01@gmail.com>
2022-04-06 16:26:18 +0200

    prova

--- phase2/interrupthandler.c
+++ phase2/interrupthandler.c
@@ -21,9 +21,10 @@ void InterruptExceptionHandler(){
     //salva il tempo iniziale dell'interrupt
     STCK(interruptstarttime);
 
-    int Cause = ((state_t*)BIOSDATAPAGE)->cause;
+    state_t* iep_s = (state_t*) BIOSDATAPAGE;    //preleviamo l'exception state
+    //int Cause = ((state_t*)BIOSDATAPAGE)->cause;
     //interruptmap = IP field of Cause register
-    int interruptmap = (Cause >> 8) & 255; //0xFF00 = CAUSEMASK
+    int interruptmap = ((iep_s->cause & 0xFF00) >> 8); //0xFF00 = CAUSEMASK
     int line = getInterruptInt(interruptmap); //calcolare la linea che ha richiesto l'interrupt
     if (line == 0) PANIC(); //caso inter- processor interrupts, disabilitato in umps3, monoprocessore
     else if (line == 1) { //PLT Interrupt
