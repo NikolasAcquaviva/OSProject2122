4ebe21c13c0f6d83d57796daea41a9f77325bdf7
Matteo Manuelli <manuelli.matteo01@gmail.com>
2022-07-29 08:43:45 +0200

    provato a castare per printare il numero di causa ed eccezione ma nada

--- phase3/sysProc.c
+++ phase3/sysProc.c
@@ -137,7 +137,23 @@ void readterminal(support_t *currSup){
 void supGeneralExceptionHandler(){
 	support_t *currSup = (support_t*) SYSCALL(GETSUPPORTPTR, 0, 0, 0);
 	/* int cause = ((currSup->sup_exceptState[GENERALEXCEPT].cause) & GETEXECCODE) >> CAUSESHIFT; */
-    int cause = CAUSE_GET_EXCCODE(currSup->sup_exceptState[GENERALEXCEPT].cause);
+    unsigned int cause = CAUSE_GET_EXCCODE(currSup->sup_exceptState[GENERALEXCEPT].cause);
+
+    //per debugging
+    char causeStr = (char) (cause >> 8);
+    /* causeStr[0] = cause & 0xFF; */
+    /* causeStr[1] = (cause >> 8) & 0xFF; */
+    char excStr = (char) (currSup->sup_exceptState[GENERALEXCEPT].reg_a0 >> 8);
+    /* excStr[0] = currSup->sup_exceptState[GENERALEXCEPT].reg_a0 & 0xFF; */
+    /* excStr[1] = (currSup->sup_exceptState[GENERALEXCEPT].reg_a0 >> 8) & 0xFF; */
+    klog_print("\nnumero causa: ");
+    klog_print(&causeStr);
+    klog_print("\n");
+    klog_print("\nnumero eccezione; ");
+    klog_print(&excStr);
+    klog_print("\n");
+
+
 	if (cause == SYSEXCEPTION){
 		switch(currSup->sup_exceptState[GENERALEXCEPT].reg_a0)
 		{
@@ -163,9 +179,7 @@ void supGeneralExceptionHandler(){
         currSup->sup_exceptState[GENERALEXCEPT].pc_epc += 4;
         currSup->sup_exceptState[GENERALEXCEPT].reg_t9 += WORDLEN; //me lo ero dimenticato...
         LDST(&(currSup->sup_exceptState[GENERALEXCEPT]));
-	}
-    //sarà dovuto ad un errore di ritorno sbagliato? non credo
-    else if (currSup->sup_exceptState[GENERALEXCEPT].reg_a0 == READTERMINAL){
+	} else if (currSup->sup_exceptState[GENERALEXCEPT].reg_a0 == READTERMINAL){ 
         klog_print("\nENTRATA STRANA\n");
         readterminal(currSup);
         currSup->sup_exceptState[GENERALEXCEPT].pc_epc += 4;
